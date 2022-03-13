f773cecb469ccfbe4d36a8919fe6c4614b73392f
nerett <pt-mvb@inbox.lv>
2022-03-14 01:31:52 +0300

    fixed HASH_KEY constant length

--- ht_fdict_config.h
+++ ht_fdict_config.h
@@ -2,7 +2,7 @@
 #define HT_DICT_CONFIG_H_INCLUDED
 
 
-const unsigned long long HASH_KEY = 0xAB0BAB0BAB0BAB0BA;
+const unsigned long long HASH_KEY = 0xAB0BAB0BAB0BAB0B;
 typedef char xor_data_t;
 
 
