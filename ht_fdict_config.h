56a7395f28b4c8dd3961dc6e374d2486ca269726
nerett <pt-mvb@inbox.lv>
2022-03-14 02:48:36 +0300

    fixed building issue

--- ht_fdict_config.h
+++ ht_fdict_config.h
@@ -6,7 +6,7 @@ const unsigned long long HASH_KEY = 0xAB0BAB0BAB0BAB0B;
 typedef char xor_data_t;
 
 
-unsigned long long xor_hash( const void* data, int size );
+unsigned long long xor_hash( const xor_data_t* data, int size );
 
 
 #endif //HT_DICT_CONFIG_H_INCLUDED
