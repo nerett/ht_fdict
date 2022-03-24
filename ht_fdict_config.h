d269df9e9b7b7f7ece158013ac20496ff3af9e2d
nerett <pt-mvb@inbox.lv>
2022-03-24 22:54:17 +0300

    finished basic hashtable feature implementation

--- ht_fdict_config.h
+++ ht_fdict_config.h
@@ -9,7 +9,7 @@ typedef unsigned long long hash_t;
 
 /*--------------------------CONST---------------------------------------------*/
 const hash_t HASH_KEY = 0xAB0BAB0BAB0BAB0B;
-const int DEFAULT_HASHTABLE_SIZE = 100; //!TODO установить значение в 50 000 - 100 000
+const int DEFAULT_HASHTABLE_SIZE = 10; //!TODO установить значение в 50 000 - 100 000
 
 
 /*--------------------------PROTOTYPE-------------------------------------------*/
--- main.cpp
+++ main.cpp
@@ -1,7 +1,23 @@
 #include "ht_fdict.h"
-
+//#include <string.h>
 
 int main()
 {
+	//strdup( NULL );
+	CFDictionary test_dictionary;
+	test_dictionary.addword( "cat" );
+	printf( "added cat\n\n" );
+	test_dictionary.addword( "mouse" );
+	printf( "added mouse\n\n" );
+	test_dictionary.addword( "bat" );
+	printf( "added bat\n\n" );
+	test_dictionary.addword( "zebra" );
+	printf( "added zebra\n\n" );
+	test_dictionary.addword( "lion" );
+	printf( "added lion\n\n" );
+	test_dictionary.addword( "tiger" );
+	printf( "added tiger\n\n" );
+
+
 	return 0;
 }
